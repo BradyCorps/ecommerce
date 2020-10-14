2865134d72e2c71a7bd5a83b9680deb97672bff6
Brady Corps <brady@bradycorps.ca>
2020-10-14 10:25:57 -0500

    react-router / router-bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,19 +7,28 @@ const Header = () => {
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 				<Container>
-					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+					<LinkContainer to="/">
+						<Navbar.Brand>ProShop</Navbar.Brand>
+					</LinkContainer>
+
 					<Nav className="ml-auto">
-						<Nav.Link className="px-2" href="#">
-							<i className="fas fa-home px-1"></i>Home
-						</Nav.Link>
+						<LinkContainer to="/">
+							<Nav.Link className="px-2">
+								<i className="fas fa-home px-1"></i>Home
+							</Nav.Link>
+						</LinkContainer>
 
-						<Nav.Link href="/cart">
-							<i className="fas fa-shopping-cart px-1"></i>Cart
-						</Nav.Link>
+						<LinkContainer to="/card">
+							<Nav.Link>
+								<i className="fas fa-shopping-cart px-1"></i>Cart
+							</Nav.Link>
+						</LinkContainer>
 
-						<Nav.Link className="px-2" href="/login">
-							<i className="fas fa-user px-1"></i>Login
-						</Nav.Link>
+						<LinkContainer to="/login">
+							<Nav.Link className="px-2" href="/login">
+								<i className="fas fa-user px-1"></i>Login
+							</Nav.Link>
+						</LinkContainer>
 					</Nav>
 				</Container>
 			</Navbar>
